ff8e18cb0eae0f3f124702b2e0829128075deae6
ibnuprtma <pibnu86@gmail.com>
2019-06-01 02:42:40 +0700

    fix spacing

--- routes/frontend.php
+++ routes/frontend.php
@@ -4,7 +4,7 @@ Route::name('frontend.')->group(function () {
     Route::group([
   
       'middleware'    => 'auth',
-      'namespace'          => 'Frontend'
+      'namespace'     => 'Frontend'
   
     ], function () {
   
