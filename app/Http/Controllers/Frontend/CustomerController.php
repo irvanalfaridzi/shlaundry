c5b794974bf3bac77fc7857001b106567015162e
ibnuprtma <pibnu86@gmail.com>
2019-06-01 22:23:53 +0700

    fix typo

--- app/Http/Controllers/Frontend/CustomerController.php
+++ app/Http/Controllers/Frontend/CustomerController.php
@@ -24,7 +24,7 @@ class CustomerController extends Controller
      */
     public function create()
     {
-        return view('frontend.supplier.create');
+        return view('frontend.customer.create');
     }
 
     /**
