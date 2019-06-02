a2cd5e007b6ed6170cecfa78a4d318bcc84796cc
ibnuprtma <pibnu86@gmail.com>
2019-06-02 13:44:04 +0700

    fix typo

--- resources/views/frontend/supplier/index.blade.php
+++ resources/views/frontend/supplier/index.blade.php
@@ -10,10 +10,10 @@
       <div class="card-header bg-white border-0">
         <div class="row align-items-center">
           <div class="col-8">
-            <h3 class="mb-0">suppliers</h3>
+            <h3 class="mb-0">Suppliers</h3>
           </div>
           <div class="col-4 text-right">
-          <a href="{{ route('frontend.supplier.create')}}" class="btn btn-sm btn-primary">Add supplier</a>
+          <a href="{{ route('frontend.supplier.create')}}" class="btn btn-sm btn-primary">Add Supplier</a>
           </div>
         </div>
       </div>
