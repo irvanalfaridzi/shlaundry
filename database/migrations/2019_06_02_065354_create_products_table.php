f5f86b8c5f86daa2b8405c4e13003445a5acce6a
ibnuprtma <pibnu86@gmail.com>
2019-06-02 14:04:57 +0700

    add table products

---
+++ database/migrations/2019_06_02_065354_create_products_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('code');
+            $table->string('name', 60);
+            $table->unsignedBigInteger('unit_id');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedInteger('price');
+            $table->timestamps();
+            $table->softDeletes();
+
+
+            $table->foreign('unit_id')
+                    ->references('id')->on('product_unit')
+                    ->onUpdate('cascade')
+                    ->onDelete('restrict');
+
+            $table->foreign('category_id')
+                    ->references('id')->on('product_category')
+                    ->onUpdate('cascade')
+                    ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
