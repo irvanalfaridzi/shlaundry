35cca5065f11f5648f180745d6de8738b0dcc760
ibnuprtma <pibnu86@gmail.com>
2019-06-11 20:48:09 +0700

    create a table

---
+++ database/migrations/2019_06_10_114700_create_employees_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('firstname');
+            $table->string('lastname')->nullable();
+            $table->string('gender')->nullable();
+            $table->string('phone');
+            $table->string('country')->nullable();
+            $table->string('city')->nullable();
+            $table->string('postalcode')->nullable();
+            $table->text('address');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
